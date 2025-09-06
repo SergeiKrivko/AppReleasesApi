ba4f3739fffc37f07a8f7df02a36b5b670fab0c0
SergeiKrivko <sergik.krivko@gmail.com>
2025-09-06 16:59:14 +0300

    BranchService

---
+++ AppReleases.Application/Services/BranchService.cs
@@ -0,0 +1,46 @@
+﻿using AppReleases.Core.Abstractions;
+using AppReleases.Core.Models;
+
+namespace AppReleases.Application.Services;
+
+public class BranchService(IBranchRepository branchRepository) : IBranchService
+{
+    public Task<IEnumerable<Branch>> GetAllBranchesAsync(Guid applicationId)
+    {
+        return branchRepository.GetAllBranchesAsync(applicationId);
+    }
+
+    public Task<Branch> GetBranchByIdAsync(Guid branchId)
+    {
+        return branchRepository.GetBranchByIdAsync(branchId);
+    }
+
+    public Task<Branch> GetBranchByNameAsync(Guid applicationId, string name)
+    {
+        return branchRepository.GetBranchByNameAsync(applicationId, name);
+    }
+
+    public async Task<Branch> CreateBranchAsync(Guid applicationId, string name)
+    {
+        var branch = new Branch
+        {
+            Id = Guid.NewGuid(),
+            ApplicationId = applicationId,
+            Name = name,
+            CreatedAt = DateTime.Now,
+            Duration = null,
+        };
+        await branchRepository.CreateBranchAsync(branch);
+        return branch;
+    }
+
+    public Task UpdateBranchAsync(Guid branchId, TimeSpan? duration)
+    {
+        return branchRepository.UpdateBranchAsync(branchId, duration);
+    }
+
+    public Task DeleteBranchAsync(Guid branchId)
+    {
+        return branchRepository.DeleteBranchAsync(branchId);
+    }
+}
---
+++ AppReleases.Core/Abstractions/IBranchService.cs
@@ -0,0 +1,13 @@
+﻿using AppReleases.Core.Models;
+
+namespace AppReleases.Core.Abstractions;
+
+public interface IBranchService
+{
+    public Task<IEnumerable<Branch>> GetAllBranchesAsync(Guid applicationId);
+    public Task<Branch> GetBranchByIdAsync(Guid branchId);
+    public Task<Branch> GetBranchByNameAsync(Guid applicationId, string name);
+    public Task<Branch> CreateBranchAsync(Guid applicationId, string name);
+    public Task UpdateBranchAsync(Guid branchId, TimeSpan? duration);
+    public Task DeleteBranchAsync(Guid branchId);
+}
