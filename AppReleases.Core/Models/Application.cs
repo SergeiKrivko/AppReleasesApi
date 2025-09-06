2486cca107d18deadf3660612cf9f4f74f8db966
SergeiKrivko <sergik.krivko@gmail.com>
2025-09-06 20:32:52 +0300

    Main branch, migrations

--- AppReleases.Core/Models/Application.cs
+++ AppReleases.Core/Models/Application.cs
@@ -8,4 +8,5 @@ public class Application
     public string? Description { get; init; }
     public required DateTime CreatedAt { get; init; }
     public DateTime? DeletedAt { get; init; }
+    public required string MainBranch { get; init; }
 }
