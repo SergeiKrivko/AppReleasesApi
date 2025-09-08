e08b90d3ceb244445caca594741a45d0e238b2b5
SergeiKrivko <sergik.krivko@gmail.com>
2025-09-08 23:57:17 +0300

    display branch in releases list

--- UI/src/app/pages/releases-page/releases-page.ts
+++ UI/src/app/pages/releases-page/releases-page.ts
@@ -1,14 +1,26 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {ReleaseService} from '../../services/release.service';
 import {AsyncPipe} from '@angular/common';
-import {ReleaseCard} from '../../components/release-card/release-card';
+import {DateFromNowPipe} from '../../pipes/date-from-now-pipe';
+import {TuiButton, TuiSurface} from '@taiga-ui/core';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {RouterLink} from '@angular/router';
+import {BranchByIdPipe} from '../../pipes/branch-by-id-pipe';
+import {TuiChip} from '@taiga-ui/kit';
 
 @Component({
   standalone: true,
   selector: 'app-releases-page',
   imports: [
     AsyncPipe,
-    ReleaseCard
+    DateFromNowPipe,
+    TuiButton,
+    TuiCardLarge,
+    TuiHeader,
+    TuiSurface,
+    RouterLink,
+    BranchByIdPipe,
+    TuiChip
   ],
   templateUrl: './releases-page.html',
   styleUrl: './releases-page.scss',
