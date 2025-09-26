87ab28e1c571f2e99add454b187736e0548c6a7e
SergeiKrivko <sergik.krivko@gmail.com>
2025-09-26 13:30:18 +0300

    fix parse prometheus response

--- AppReleases.Api/Helpers/MetricsRepository.cs
+++ AppReleases.Api/Helpers/MetricsRepository.cs
@@ -13,11 +13,6 @@ public class MetricsRepository : IMetricsRepository
 
     private readonly string? _prometheusContainer = Environment.GetEnvironmentVariable("PROMETHEUS_CONTAINER");
 
-    private JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    };
-
     public Task<IEnumerable<Metric>> GetMetricsAsync(string query, DateTime timestamp)
     {
         return RequestMetricsAsync($"api/v1/query" +
@@ -33,6 +28,7 @@ public class MetricsRepository : IMetricsRepository
 
     private async Task<IEnumerable<Metric>> RequestMetricsAsync(string url)
     {
+        Console.WriteLine(url);
         var response = await _httpClient.GetFromJsonAsync<PrometheusResponse>(url);
         if (response is null)
             throw new Exception("Prometheus response is unprocessable");
@@ -41,7 +37,7 @@ public class MetricsRepository : IMetricsRepository
             .Select(m => new Metric
             {
                 Fields = m.Metric,
-                Name = m.Metric["__name__"],
+                Name = m.Metric.GetValueOrDefault("__name__"),
                 Timestamp = DateTime.UnixEpoch + TimeSpan.FromSeconds(m.Value[0].GetDouble()),
                 Value = m.Value[1].GetString(),
             });
--- AppReleases.Models/Metric.cs
+++ AppReleases.Models/Metric.cs
@@ -3,7 +3,7 @@
 public class Metric
 {
     public required Dictionary<string, string> Fields { get; set; }
-    public required string Name { get; set; }
+    public string? Name { get; set; }
     public DateTime Timestamp { get; set; }
     public string? Value { get; set; }
 }
